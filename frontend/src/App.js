02cbbf944b4a82132869ab48655da6ac730dafdf
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-04-21 15:11:57 +0300

    creating react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
