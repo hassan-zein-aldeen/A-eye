fb98aa52f2254e2e7c00b9a3a0bf5d1bf67ef128
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-04-22 18:11:28 +0300

    adding video bacjground

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import Header from "../../components/Header/header";
+import Video from "../../video/high.mp4";
+import './home.css';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <video className="back_video" src={Video} autoPlay loop muted></video>
     </div>
   );
 }
