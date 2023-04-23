3acfa97e83dd4a0e4a5dba82f77f17efa07183fd
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-04-23 21:57:11 +0300

    inserting reset file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
+import './reset.css';
 import './App.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './pages/Home';
+
 
 function App() {
   return (
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -10,6 +10,24 @@ const Home = () => {
         <video className="back_video" src={Video} autoPlay loop muted></video>
         <p className="vid_title">Audience <br></br> Optimization</p>
         <p className="vid_text">"Targeting the right audience ensures <br></br>that your advertising efforts are not wasted."</p>
+        <div className="directions">
+          <table>
+            <tr>
+              <td id="firstcell">
+              <a id="ads" href="http://localhost:3000/">Add Ads</a>
+              </td>
+              <td>
+              <a id="offers" href="http://localhost:3000/">Add Offers</a>
+              </td>
+              <td>
+              <a id="results" href="http://localhost:3000/">Show Results</a>
+              </td>
+              <td>
+              <a id="plans" href="http://localhost:3000/">AI Plan</a>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     </>
   );
