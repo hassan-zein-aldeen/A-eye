e44d16c15dcfba573b82bf2f83c04b7548257dfe
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-04-29 22:18:14 +0300

    backend: connecting to database

---
+++ backend/configs/db.config.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection;
+db.on("error", (err)=>{
+  console.error(err);
+})
+
+db.once("open", ()=>{
+  console.log("mongo db connected successfully");
+})
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-app.listen(process.env.PORT || 30001 , ()=>{
+app.listen(process.env.PORT || 30001 , (err)=>{
+  if(err) console.error(err)
   console.log("Server is running on port", process.env.PORT);
+  require("./configs/db.config");
 });
