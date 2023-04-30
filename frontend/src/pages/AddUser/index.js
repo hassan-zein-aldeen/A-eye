d03f1341174dc78eef46094d619742e08ec50889
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-04-30 10:43:45 +0300

    frontend: creating input validations

--- frontend/src/pages/AddUser/index.js
+++ frontend/src/pages/AddUser/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "./axios";
+import axios from "axios";
 import Header from "../../components/Header/header";
 import './addUser.css';
 import Button1 from "../../components/Button1/Button";
@@ -8,13 +8,20 @@ const AddUser = () => {
 
   const [activeDiv, SetActiveDiv] = useState("requests");
   const [actveLink, SetActiveLink] = useState('#requests');
+
   const [role, setRole] = useState("");
-  const [shopname, SetShopname] = useState("");
+  const [shopname, setShopname] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
 
+  const [errrole, seterrRole] = useState("");
+  const [errshopname, seterrShopname] = useState("");
+  const [errusername, seterrUsername] = useState("");
+  const [errpassword, seterrPassword] = useState("");
+  const [erremail, seterrEmail] = useState("");
+  const [erraddress, seterrAddress] = useState("");
 
   const validateEmail = (email) => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -23,14 +30,51 @@ const AddUser = () => {
 
 
 
-  function handleClick(event) {
+  const addNewUser = async (e) => {
+    const form = e.target;
+    seterrRole("");
+    seterrShopname("");
+    seterrUsername("");
+    seterrPassword("");
+    seterrEmail("");
+    seterrAddress("");
+    e.preventDefault();
 
+    if (!role) {
+      seterrRole("user's role is missing");
+      return
+    }
+
+    if (!shopname) {
+      seterrShopname("shopname missing");
+    }
+
+    if (!username) {
+      seterrUsername("username missing");
+    }
+
+    if (password.length < 6) {
+      seterrPassword("password at least 6 characters");
+    }
+
+    if (!email) {
+      seterrEmail("email missing");
+    } else if (!validateEmail(email)) {
+      seterrEmail("Enter valid email")
+    }
+
+    if (!address) {
+      seterrAddress("address missing");
+    }
+
+  }
+
+  function handleClick(event) {
     event.preventDefault();
     const target = event.target.getAttribute("data-target");
     SetActiveDiv(target);
     SetActiveLink(event.target.hash);
   }
-
 
 
   return (
@@ -55,15 +99,16 @@ const AddUser = () => {
             </div>
           </div>}
           {activeDiv === "create" && <div id="new_user">
-            <p className="form_title">New User</p>
+            <p className="form_title">New User {errrole} {errshopname} {errusername} {errpassword} {erremail} {erraddress}</p>
+
             <div className="fields">
               <div className="user_type">
                 <label>
-                  <input type="radio" name="type" value="user" id="user" />
+                  <input type="radio" name="type" value="user" id="user" onChange={(e) => setRole(e.target.value)} />
                   User
                 </label>
                 <label>
-                  <input type="radio" name="type" value="admin" id="admin" />
+                  <input type="radio" name="type" value="admin" id="admin" onChange={(e) => setRole(e.target.value)} />
                   Admin
                 </label>
               </div>
@@ -73,19 +118,17 @@ const AddUser = () => {
                   <label for="username">Username: </label>
                   <label for="password">Password: </label>
                   <label for="email">Email: </label>
-                  <label for="phone">Phone Number: </label>
                   <label for="address">Address: </label>
                 </div>
                 <div className="inputs">
-                  <input type="text" id="shop_name" placeholder="Enter user's shop name" />
-                  <input type="text" id="username" placeholder="Enter username" />
-                  <input type="text" id="password" placeholder="Enter password" />
-                  <input type="email" id="email" placeholder="Enter user's email" />
-                  <input type="text" id="phone" placeholder="example(00-000000)" />
-                  <input type="text" id="address" placeholder="example(City/Mall/Floor)" />
+                  <input type="text" id="shop_name" placeholder="Enter user's shop name" onChange={(e) => setShopname(e.target.value)} required />
+                  <input type="text" id="username" placeholder="Enter username" onChange={(e) => setUsername(e.target.value)} />
+                  <input type="text" id="password" placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
+                  <input type="email" id="email" placeholder="Enter user's email" onChange={(e) => setEmail(e.target.value)} />
+                  <input type="text" id="address" placeholder="example(City/Mall/Floor)" onChange={(e) => setAddress(e.target.value)} />
                 </div>
               </div>
-              <Button1>Create</Button1>
+              <Button1 onClick={addNewUser}>Create</Button1>
             </div>
           </div>}
         </div>
