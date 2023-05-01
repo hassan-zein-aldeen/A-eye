9719f6c988440f0b0951e76f0d0a8fce3cdd2b1e
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-01 22:45:11 +0300

    backend: changing receiver to array, message model

--- backend/models/messagesModel.js
+++ backend/models/messagesModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-  receiver: {
+  receiver: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required:true
-  },
+  }],
   sender: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
