448625978c95bdc6a96a1f75ed8603d5d7885869
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-03 00:03:51 +0300

    frontend: creating active divs for message section

--- frontend/src/components/Message/index.js
+++ frontend/src/components/Message/index.js
@@ -6,12 +6,14 @@ import Button1 from '../Button1/Button';
 
 const Message = () => {
 
-  const [activeDiv, setActiveDiv] = useState();
+  const [activeDiv, setActiveDiv] = useState('old_messages');
+  const [actveLink, setActiveLink] = useState('');
 
   const handleClick = async (event) => {
     event.preventDefault();
     const target = event.target.getAttribute("data-target");
     setActiveDiv(target);
+    setActiveLink(event.target.hash);
   }
 
 
@@ -20,29 +22,31 @@ const Message = () => {
       <div className='header_title'>
         <div className='linking'>
           <p className='mess_title'>Send Message</p>
-          <a href='#' id='history' >History</a>
+          <a href='#' onClick={handleClick} data-target='old_messages' id='history'>History</a>
         </div>
-        <a href='#' id='message'>Send Message</a>
+        <a href='#' onClick={handleClick} data-target='mess_content' id='message'>Send Message</a>
       </div>
-      <div className='mess_content'>
-        <div className='message_header'>
-          <div className='mess_labels'>
-            <label>Shop Name (s):</label>
-            <label>Title:</label>
-            <label>Text:</label>
+      {activeDiv === "old_messages" && <div id="old_messages">This is first section</div>}
+      {activeDiv === "mess_content" &&
+        <div className='mess_content'>
+          <div className='message_header'>
+            <div className='mess_labels'>
+              <label>Shop Name (s):</label>
+              <label>Title:</label>
+              <label>Text:</label>
+            </div>
+            <div className='mess_inputs'>
+              <input id='shopname_input' type='text' placeholder='Enter Shop(s) Name' />
+              <input id='title_input' type='text' placeholder='Enter message Title' />
+            </div>
           </div>
-          <div className='mess_inputs'>
-            <input id='shopname_input' type='text' placeholder='Enter Shop(s) Name' />
-            <input id='title_input' type='text' placeholder='Enter message Title' />
+          <div className='message_box'>
+            <textarea id='text_input' placeholder='Insert Your Message' />
           </div>
-        </div>
-        <div className='message_box'>
-          <textarea id='text_input' placeholder='Insert Your Message' />
-        </div>
-        <div className='send_but'>
-          <Button1>Send</Button1>
-        </div>
-      </div>
+          <div className='send_but'>
+            <Button1>Send</Button1>
+          </div>
+        </div>}
     </div>
   )
 }
