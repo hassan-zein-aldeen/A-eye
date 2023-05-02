65f99ca14f5e368a4d6b77b2773fbc7212306326
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-02 14:48:26 +0300

    backend: adding validations to send message api

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -6,15 +6,25 @@ exports.sendMessage = async (req, res) => {
 
   try {
     const users = await User.find({}, { '_id': 1 });
+    const userIds = users.map(user => user._id.toString());
+    const isfound = rec.every(element => userIds.includes(element));
 
-    const message = new Message();
-    message.receiver = rec;
-    message.sender = sender;
-    message.title = title;
-    message.txtContent = txtContent;
-    await message.save();
+    function hasDuplicates(array) {
+      return new Set(array).size !== array.length;
+    }
 
+    if (isfound && !hasDuplicates(rec)) {
+      const message = new Message();
+      message.receiver = rec;
+      message.sender = sender;
+      message.title = title;
+      message.txtContent = txtContent;
+      await message.save();
+      res.json(message);
+    } else {
+      res.json({ message: "Not allowed to send message for duplicated user or user not found" });
+    }
   } catch (error) {
-    res.json({ message: "Error while sending message", error });
+    res.json({ message: "Error while sending message Status", error });
   }
 }
