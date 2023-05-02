764ecf7fb79ad15ec274f3f49661b54da0534c57
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-02 14:47:00 +0300

    backend: creating message Api for admin

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -1,1 +1,20 @@
 const Message = require("../models/messagesModel");
+const User = require("../models/userModel");
+
+exports.sendMessage = async (req, res) => {
+  const { rec, sender, title, txtContent } = req.body;
+
+  try {
+    const users = await User.find({}, { '_id': 1 });
+
+    const message = new Message();
+    message.receiver = rec;
+    message.sender = sender;
+    message.title = title;
+    message.txtContent = txtContent;
+    await message.save();
+
+  } catch (error) {
+    res.json({ message: "Error while sending message", error });
+  }
+}
