2b6a136d08861361edc67200c99444c1ae3889f0
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-02 23:41:24 +0300

    frontend: designing messages pages

--- frontend/src/components/Message/index.js
+++ frontend/src/components/Message/index.js
@@ -5,9 +5,25 @@ import axios from "axios";
 import Button1 from '../Button1/Button';
 
 const Message = () => {
+
+  const [activeDiv, setActiveDiv] = useState();
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+    const target = event.target.getAttribute("data-target");
+    setActiveDiv(target);
+  }
+
+
   return (
     <div>
-      <p className='mess_title'>Send Message</p>
+      <div className='header_title'>
+        <div className='linking'>
+          <p className='mess_title'>Send Message</p>
+          <a href='#' id='history' >History</a>
+        </div>
+        <a href='#' id='message'>Send Message</a>
+      </div>
       <div className='mess_content'>
         <div className='message_header'>
           <div className='mess_labels'>
