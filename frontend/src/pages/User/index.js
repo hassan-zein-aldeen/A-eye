1d74e032442bfdd5dcc6a7de95de0626cb8e51f4
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-05 20:00:10 +0300

    frontend: dividing the page of user into sections

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -18,9 +18,43 @@ const User = () => {
           <Userbar handleAnchorClick={handleAnchorClick} />
         </div>
         <div className="content">
-          {activeDiv === "ads" && <div id="ads" style={{ color: "black" }}>
+
+
+
+          {/*Start of Ads div */}
+          {activeDiv === "ads" && <div id="ads">
             Hello from Creating ads
           </div>}
+          {/*End of Ads div */}
+
+
+
+
+          {/*Start of requesting ads div */}
+          {activeDiv === "user_requests" && <div id="user_requests" >
+            Hello from requesting add
+          </div>}
+          {/*End of requesting ads div */}
+
+
+
+          {/*Start of messages div */}
+          {activeDiv === "user_messages" && <div id="user_messages">
+            Hello from messages of user
+          </div>}
+          {/*End of messages div */}
+
+
+
+
+          {/*Start of AI tools div */}
+          {activeDiv === "ai_tool" && <div id="ai_tool">
+            Hello from AI tools of user
+          </div>}
+          {/*End of of AI tools div */}
+
+
+
         </div>
       </div>
     </div>
