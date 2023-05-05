2fc5405ace446f94bd22d4f2929a4a41e1350e61
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-05 23:55:34 +0300

    frontend: linking api get message to user

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 import "./user.css";
 import Userbar from "../../components/Userbar";
+import axios from "axios";
 
 
 const User = () => {
   const [activeDiv, setActiveDiv] = useState('');
+  const [adminMessage, setAdminMessage] = useState('');
   const userShopNameTitle = localStorage.getItem("shopname");
+  const receiverId = localStorage.getItem('id').toString();
+
+  const getMessages = async (senderId) => {
+    try {
+      const response = await axios.get(`http://127.0.0.1:3000/message/${receiverId}`);
+      const admin_message = response.data;
+      setAdminMessage(admin_message);
+      console.log(adminMessage);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 
   const handleAnchorClick = (e, target) => {
     e.preventDefault();
     setActiveDiv(target);
   };
 
-  
+
+
 
   return (
     <div>
@@ -44,9 +60,10 @@ const User = () => {
           {/*Start of messages div */}
           {activeDiv === "user_messages" && <div id="user_messages">
             <div className="messages">
-              <p className="section_title">
-                All Messages
-              </p>
+              <a href="#" className="message_title" onClick={getMessages}>
+                Show Messages
+              </a>
+              
             </div>
           </div>}
           {/*End of messages div */}
