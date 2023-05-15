6d5d6c140b0417a54d638264745099eb4a311889
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-05-15 12:26:43 +0300

    frontend: editing style of create user for admin

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -22,14 +22,10 @@ const Admin = () => {
   const [address, setAddress] = useState("");
   const [result, setResult] = useState("");
 
-  const [errrole, seterrRole] = useState("");
-  const [errshopname, seterrShopname] = useState("");
-  const [errusername, seterrUsername] = useState("");
-  const [errpassword, seterrPassword] = useState("");
-  const [erremail, seterrEmail] = useState("");
-  const [erraddress, seterrAddress] = useState("");
+  const [createUserErr, setCreateUserErr] = useState("");
 
   const [res_message, setRes_message] = useState("");
+  const [activeAnchor, setActiveAnchor] = useState("users");
 
   const [pendingResults, setPendingResults] = useState("");
   const [activeResults, setActiveResults] = useState("");
@@ -56,21 +52,33 @@ const Admin = () => {
   }
   ]);
 
-  const handleSubUserAccounts = (e) => {
+  const handleSubUserAccounts = (e, target) => {
     e.preventDefault();
-    const target = e.target.getAttribute("data-target");
-    setActiveDiv(true);
-    setIsActive(false);
+    setActiveAnchor(target)
   }
 
+
+  useEffect(() => {
+    console.log('div', activeDiv);
+    console.log(' anchor  ', activeAnchor);
+  }, [activeDiv, activeAnchor]);
+
+  const handleCreateUserAccounts = (e, target) => {
+    e.preventDefault();
+    const newTarget = e.target.getAttribute("data-target");
+    setActiveAnchor(newTarget);
+    setActiveDiv(newTarget);
+    console.log(newTarget);
+    console.log('active div is ', newTarget);
+    console.log('active anchor is ', newTarget)
+  }
 
   const handleDefaultClick = (e) => {
     const target = e.target.getAttribute("data-target");
-    setIsActive(true);
     setActiveDiv(target);
+    setActiveLink(e.target.hash);
+    setIsActive(true);
   }
-
-
 
   const handleClick = async (event) => {
     const target = event.target.getAttribute("data-target");
@@ -124,33 +132,34 @@ const Admin = () => {
     return () => { if (timeout) { clearTimeout(timeout); } };
   }, [res_message]);
 
+  useEffect(() => {
+    let timeout = null;
+    if (createUserErr) { timeout = setTimeout(() => { setCreateUserErr(''); }, 2000) }
+    return () => { if (timeout) { clearTimeout(timeout); } };
+  }, [createUserErr]);
+
   const addNewUser = async (e) => {
     e.preventDefault();
 
-    seterrRole("");
-    seterrShopname("");
-    seterrUsername("");
-    seterrPassword("");
-    seterrEmail("");
-    seterrAddress("");
+    setCreateUserErr("");
 
     if (!role) {
-      seterrRole("user's role is missing");
+      setCreateUserErr("user's role is missing");
       return;
     }
 
     if (!shopname) {
-      seterrShopname("shopname missing");
+      setCreateUserErr("shopname missing");
       return;
     }
 
     if (!username) {
-      seterrUsername("username missing");
+      setCreateUserErr("username missing");
       return;
     }
 
     if (password.length < 6) {
-      seterrPassword("password at least 6 characters");
+      setCreateUserErr("password at least 6 characters");
       return;
     }
 
@@ -160,15 +169,15 @@ const Admin = () => {
     };
 
     if (!email) {
-      seterrEmail("email missing");
+      setCreateUserErr("email missing");
       return;
     } else if (!validateEmail(email)) {
-      seterrEmail("Enter valid email")
+      setCreateUserErr("Enter valid email")
       return;
     }
 
     if (!address) {
-      seterrAddress("address missing");
+      setCreateUserErr("address missing");
       return;
     }
 
@@ -310,53 +319,62 @@ const Admin = () => {
 
           {activeDiv != "displayAccounts" && <div>
             {activeDiv != "s_notif" && <div>
-              <div className="AllUserrequests">
-                <p className="titlesectionAds">All Users' Ads</p>
-                <div className="directionalAdmin_links">
-                  <a href="#"
-                    onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="activeAds">Active</a>
-                  <a href="#"
-                    onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="pendingAds">Pending</a>
-                  <a href="#"
-                    onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="inActiveAds">InActive</a>
-                  <a href="#"
-                    onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="rejectedAds">Rejected</a>
-                </div>
-                {isActive && <div className="isActive">
-                  <div className="adminContentControle">
-                    {userReqResult.map((result) => (
-                      <div key={result._id} className="shownAdminResultCard">
-                        <div className="respActiveCard">
-                          <div className="imageSection" style={{
-                            backgroundImage: `url(http://127.0.0.1:3000/uploads/${result.image})`
-                          }}>
-                          </div>
-                          <div className="respActiveCard_text">
-                            <h4>{result.title}</h4>
-                            <p>{result.description}</p>
+              {activeDiv != "createNewUser" && <div>
+                <div className="AllUserrequests">
+                  <p className="titlesectionAds">All Users' Ads</p>
+                  <div className="directionalAdmin_links">
+                    <a href="#"
+                      onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="activeAds">Active</a>
+                    <a href="#"
+                      onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="pendingAds">Pending</a>
+                    <a href="#"
+                      onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="inActiveAds">InActive</a>
+                    <a href="#"
+                      onClick={(e) => { handleClick(e); getAllRequests(e) }} data-target="rejectedAds">Rejected</a>
+                  </div>
+                  {isActive && <div className="isActive">
+                    <div className="adminContentControle">
+                      {userReqResult.map((result) => (
+                        <div key={result._id} className="shownAdminResultCard">
+                          <div className="respActiveCard">
+                            <div className="imageSection" style={{
+                              backgroundImage: `url(http://127.0.0.1:3000/uploads/${result.image})`
+                            }}>
+                            </div>
+                            <div className="respActiveCard_text">
+                              <h4>{result.title}</h4>
+                              <p>{result.description}</p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>}
-              </div>
+                      ))}
+                    </div>
+                  </div>}
+                </div>
+              </div>}
             </div>}
+
           </div>}
+
           {activeDiv === "s_notif" && <div id="nofitication">
             <Message />
           </div>}
-
 
           {activeDiv === "displayAccounts" && <div id="displayAccounts">
             <div className="display_users">
               <p className="UsersAccouns">All Accounts</p>
               <div className="acc_title">
                 <div className="acc_sublinks">
-                  <a href="#" onClick={handleSubUserAccounts} data-target="active_users">Active</a> {/*click to show */}
-                  <a href="#" onClick={handleDefaultClick} data-target="inActive_users">Inactive</a>
+                  <a href="#"
+                    className={activeAnchor === 'active_users' ? 'active' : ''}
+                    onClick={(e) => handleSubUserAccounts(e, 'active_users')} data-target="active_users">Active</a> {/*click to show */}
+                  <a href="#"
+                    className={activeAnchor === 'inActive_users' ? 'active' : ''}
+                    onClick={(e) => handleSubUserAccounts(e, 'inActive_users')} data-target="inActive_users">Inactive</a>
                 </div>
-                <button id="create_user" href="#" onClick={handleDefaultClick} data-target="create">Create User</button>
+                <button id="create_user" href="#"
+                  className={activeAnchor === 'createNewUser' ? 'active' : ''}
+                  onClick={(e) => handleCreateUserAccounts(e, 'createNewUser')} data-target="createNewUser">Create User</button>
               </div>
               <div>
                 <table className="users_table">
@@ -369,7 +387,7 @@ const Admin = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {users && users.map(user => (
+                    {activeAnchor === 'users' && users && users.map(user => (
                       <tr key={user._id}>
                         <td><img className="userimage" src={userIcon} /></td>
                         <td className="key">{user.username}</td>
@@ -377,116 +395,78 @@ const Admin = () => {
                         <td id="last_bord">{user.email}</td>
                       </tr>
                     ))}
-                    {activeDiv === "active_users" && <div id="active_users">
-                      {activeUsers && activeUsers.map(user => (
-                        <tr key={user._id}>
-                          <td><img className="userimage" src={userIcon} /></td>
-                          <td className="key">{user.username}</td>
-                          <td>{user.shopname}</td>
-                          <td id="last_bord">{user.email}</td>
-                        </tr>
-                      ))}
-                    </div>}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-
-            {activeDiv === "create" && <div id="create">
-              <p className="shopname-title">Shopname: <span> {title_name}</span></p>
-              <p className="form_title">New User</p>
-              <div className="fields">
-                <div className="user_type">
-                  <label> User type :
-                    <input type="radio" name="type" value="user" id="user"
-                      onChange={(e) => setRole(e.target.value)} />
-                    User
-                  </label>
-                  <label>
-                    <input type="radio" name="type" value="admin" id="admin"
-                      onChange={(e) => setRole(e.target.value)} />
-                    Admin
-                  </label>
-                </div>
-                <div className="add_form">
-                  <div className="labels">
-                    <label for="shop_name">Shop Name: </label>
-                    <label for="username">Username: </label>
-                    <label for="password">Password: </label>
-                    <label for="email">Email: </label>
-                    <label for="address">Address: </label>
-                  </div>
-                  <div className="inputs">
-                    <input type="text" id="shop_name" placeholder="Enter user's shop name"
-                      onChange={(e) => setShopname(e.target.value)} required />
-                    <input type="text" id="username" placeholder="Enter username"
-                      onChange={(e) => setUsername(e.target.value)} />
-                    <input type="text" id="password" placeholder="Enter password"
-                      onChange={(e) => setPassword(e.target.value)} />
-                    <input type="email" id="email" placeholder="Enter user's email"
-                      onChange={(e) => setEmail(e.target.value)} />
-                    <input type="text" id="address" placeholder="example(City/Mall/Floor)"
-                      onChange={(e) => setAddress(e.target.value)} />
-                  </div>
-                </div>
-                <div className="valid_input">
-                  <span>{errrole}</span>
-                  <span>{errshopname}</span>
-                  <span>{errusername}</span>
-                  <span>{errpassword}</span>
-                  <span>{erremail}</span>
-                  <span>{erraddress}</span>
-                  <span className="response_result"
-                    style={{
-                      backgroundColor: res_message === "User Created Successfully" ? "green" : "red",
-                      fontSize: "1.5rem",
-                      borderRadius: "0.5rem"
-                    }}>
-                    {res_message && <p>{res_message}</p>}</span>
-                </div>
-                <Button1 onClick={addNewUser}>Create</Button1>
-              </div>
-            </div>}
-
-            {activeDiv === "active_users" && <div id="active_users">
-              <p className="shopname-title">Shopname: <span> {title_name}</span></p>
-              <div className="display_users">
-                <div className="acc_title">
-                  <div className="acc_sublinks">
-                  </div>
-                  <a id="create_user" href="#" onClick={handleClick} data-target="create">Create User</a>
-                </div>
-                <span className="response_result"
-                  style={{
-                    backgroundColor: res_message === "User Status updated Successfully" ? "green" : "red",
-                    color: "white",
-                    fontSize: "1.5rem",
-                  }}>
-                  {res_message && <p>{res_message}</p>}</span>
-                <table className="users_table">
-                  <thead>
-                    <tr className="col_titles">
-                      <th></th>
-                      <th>Username</th>
-                      <th>Shop Name</th>
-                      <th>Email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {inActiveUsers && inActiveUsers.map(inActiveUser => (
-                      <tr key={inActiveUser._id}>
+                    {activeAnchor === 'active_users' && activeUsers && activeUsers.map(user => (
+                      <tr key={user._id}>
                         <td><img className="userimage" src={userIcon} /></td>
-                        <td className="key">{inActiveUser.username}</td>
-                        <td>{inActiveUser.shopname}</td>
-                        <td>{inActiveUser.email}</td>
-                        <td><Button1 onClick={() => updateStatus(inActiveUser._id)}>Activate</Button1></td>
+                        <td className="key">{user.username}</td>
+                        <td>{user.shopname}</td>
+                        <td id="last_bord">{user.email}</td>
+                        <td><a onClick={() => updateStatus(user._id)}>Deactivate</a></td> {/*try to solve */}
+                      </tr>
+                    ))}
+                    {activeAnchor === 'inActive_users' && inActiveUsers && inActiveUsers.map(user => (
+                      <tr key={user._id}>
+                        <td><img className="userimage" src={userIcon} /></td>
+                        <td className="key">{user.username}</td>
+                        <td>{user.shopname}</td>
+                        <td id="last_bord">{user.email}</td>
+                        <td><a onClick={() => updateStatus(user._id)}>Activate</a></td> {/*try to solve */}
                       </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
-            </div>}
+            </div>
           </div>}
+          {activeDiv === "createNewUser" && (<div id="createNewUser">
+            <p className="form_title">New User</p>
+            <div className="fields">
+              <div className="user_type">
+                <label> User type :
+                  <input type="radio" name="type" value="user" id="user"
+                    onChange={(e) => setRole(e.target.value)} />
+                  User
+                </label>
+                <label>
+                  <input type="radio" name="type" value="admin" id="admin"
+                    onChange={(e) => setRole(e.target.value)} />
+                  Admin
+                </label>
+              </div>
+              <div className="add_form">
+                <div className="labels">
+                  <label for="shop_name">Shop Name: </label>
+                  <label for="username">Username: </label>
+                  <label for="password">Password: </label>
+                  <label for="email">Email: </label>
+                  <label for="address">Address: </label>
+                </div>
+                <div className="inputs">
+                  <input type="text" id="shop_name" placeholder="Enter user's shop name"
+                    onChange={(e) => setShopname(e.target.value)} required />
+                  <input type="text" id="username" placeholder="Enter username"
+                    onChange={(e) => setUsername(e.target.value)} />
+                  <input type="text" id="password" placeholder="Enter password"
+                    onChange={(e) => setPassword(e.target.value)} />
+                  <input type="email" id="email" placeholder="Enter user's email"
+                    onChange={(e) => setEmail(e.target.value)} />
+                  <input type="text" id="address" placeholder="example(City/Mall/Floor)"
+                    onChange={(e) => setAddress(e.target.value)} />
+                </div>
+              </div>
+              <div className="valid_input">
+                <span className="response_result"
+                  style={{
+                    backgroundColor: res_message === "User Created Successfully" ? "green" : "red",
+                    fontSize: "1.5rem",
+                    borderRadius: "0.5rem"
+                  }}>
+                  {res_message && <p>{res_message}</p>}</span>
+                  <p style={{color:"red"}}>{createUserErr}</p>
+              </div>
+              <Button1 onClick={addNewUser}>Create</Button1>
+            </div>
+          </div>)}
 
 
           {activeDiv === "activeAds" && <div id="activeAds">
